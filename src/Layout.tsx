80e5f00c090c7b11cf64f42f12c34a06e20a6865
iamXOD <iamxod.dev@gmail.com>
2021-10-23 15:44:21 -0400

    ✨ feat: add Layout component

---
+++ src/Layout.tsx
@@ -0,0 +1,44 @@
+import { ExitToAppRounded, Help } from "@mui/icons-material";
+import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+function Layout() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar
+                position='static'
+                sx={{
+                    background:
+                        "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(180,2,2,1) 100%)",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                }}>
+                <Toolbar>
+                    <Grid>
+                        <Typography variant='h3' sx={{ color: "#990000" }}>
+                            Praeco
+                        </Typography>
+                        <Typography
+                            variant='subtitle1'
+                            sx={{ color: "#990000" }}>
+                            Sistema de información al publico
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        sx={{
+                            marginLeft: "auto",
+                            display: "inline-list-item",
+                        }}>
+                        <Typography variant='subtitle1' sx={{ margin: 1 }}>
+                            Super Administrator
+                        </Typography>
+                        <Help sx={{ margin: 1 }} />
+                        <ExitToAppRounded sx={{ margin: 1 }} />
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+export default Layout;
